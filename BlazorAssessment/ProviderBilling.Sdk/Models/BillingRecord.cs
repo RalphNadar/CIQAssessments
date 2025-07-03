51a481c7a5cac5857c4384ef7a329e3206c166e3
Kyle <cksmith2028@yahoo.com>
2025-07-02 19:08:14 -0500

    addition of sdk

---
+++ BlazorAssessment/ProviderBilling.Sdk/Models/BillingRecord.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderBilling.Sdk.Models
+{
+    public class BillingRecord
+    {
+        public string NPI { get; set; }
+        public string HCPCSCode { get; set; }
+        public string HCPCSDescription { get; set; }
+        public string PlaceOfService { get; set; }
+        public int NumberOfServices { get; set; }
+        public decimal TotalMedicarePayment { get; set; }
+        public Provider? Provider { get; set; }
+    }
+}
---
+++ BlazorAssessment/ProviderBilling.Sdk/Models/Provider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderBilling.Sdk.Models
+{
+    public class Provider
+    {
+        public string NPI { get; set; }
+        public string ProviderName { get; set; }
+        public string Specialty { get; set; }
+        public string State { get; set; }
+        public List<BillingRecord>? BillingRecords { get; set; }
+    }
+}
