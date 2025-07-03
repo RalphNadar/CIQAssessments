7df1be01a18baa08dce74bc93de1867284a92cab
Kyle <cksmith2028@yahoo.com>
2025-07-02 19:36:19 -0500

    Following addition of a couple Blazor Components

---
+++ BlazorAssessment/ProviderBillingBlazor/Controllers/RepoController.cs
@@ -0,0 +1,79 @@
+﻿using ProviderBilling.Sdk.Models;
+using Microsoft.AspNetCore.Mvc;
+using DataIngestionConsole.Domain;
+
+namespace ProviderBillingBlazor.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RepoController : Controller
+    {
+        private readonly IMedicalBillingQuery _query;
+
+        public RepoController(IMedicalBillingQuery query, MedicalBillingContext context)
+        {
+            _query = query;
+        }
+
+
+        [HttpGet("GetProviders")]
+        public async Task<ActionResult<List<ProviderBilling.Sdk.Models.Provider>>> GetProviders()
+        {
+            try
+            {
+                var providers = await _query.GetAllProviders();
+                return providers;
+            }
+            catch (Exception ex)
+            {
+                var error = ex;
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("GetRecords")]
+        public async Task<ActionResult<List<ProviderBilling.Sdk.Models.BillingRecord>>> GetRecords()
+        {
+            try
+            {
+                var records = await _query.GetAllBillingRecords();
+                return records;
+            }
+            catch (Exception ex)
+            {
+                var error = ex;
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("GetProvNPI/{NPI}")]
+        public async Task<ActionResult<ProviderBilling.Sdk.Models.Provider>> GetProvNPI(string NPI)
+        {
+            try
+            {
+                var provider = await _query.GetProvider(NPI);
+                return Ok(provider);
+            }
+            catch (Exception ex)
+            {
+                var error = ex;
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("GetRecNPI/{NPI}")]
+        public async Task<ActionResult<ProviderBilling.Sdk.Models.BillingRecord>> GetRecNPI(string NPI)
+        {
+            try
+            {
+                var record = await _query.GetBillingRecord(NPI);
+                return Ok(record);
+            }
+            catch (Exception ex)
+            {
+                var error = ex;
+                return BadRequest();
+            }
+        }
+    }
+}
--- BlazorAssessment/ProviderBillingBlazor/Program.cs
+++ BlazorAssessment/ProviderBillingBlazor/Program.cs
@@ -1,10 +1,32 @@
+using DataIngestionConsole.Domain;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using ProviderBillingBlazor.Components;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+//builder.Services.AddHttpClient();
+builder.Services.AddControllers();
+// Register HttpClient
+builder.Services.AddScoped(sp =>
+{
+    NavigationManager navigation = sp.GetRequiredService<NavigationManager>();
+    return new HttpClient { BaseAddress = new Uri(navigation.BaseUri) };
+});
+
+builder.Services.AddDbContext<MedicalBillingContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection")));
+
+builder.Services.AddTransient<IMedicalBillingQuery, MedicalBillingQuery>();
+
 
 var app = builder.Build();
 
@@ -16,11 +38,15 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 
+
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapBlazorHub();
 app.UseStaticFiles();
 app.UseAntiforgery();
-
+app.MapControllers();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
